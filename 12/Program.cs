04591337b49b337e2b21a045b2c467faf34e36ce
karimova1791 <kristina.karimova.02@list.ru>
2019-11-18 14:16:37 +0300

    first commit

---
+++ 12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Пример_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+
+            if (x < b && x >= 0)
+            {
+                Console.WriteLine(Math.Pow(x, 2) - 1 / 4 * (Math.Pow((Math.Sqrt(b + Math.Pow(x, 3))), 3)));
+            }
+
+            else if (x < a && x >= b)
+            {
+                Console.WriteLine(Math.Pow((b + a * x), 2) + b * Math.Exp(a + 1));
+
+
+            }
+            else if (x >= a)
+            {
+                Console.WriteLine(Math.Atan((Math.Pow(b, 2) + (a * x))) - (Math.Sqrt(a) * Math.Log(Math.Abs(x))));
+
+            }
+        }
+    }
+}
+
